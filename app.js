7dd754d401cdd28a2e4055955f7741190a1a12ae
Must <emailme>
2023-06-08 18:54:07 +0200

    incomes connected with result area

--- app.js
+++ app.js
@@ -1,29 +1,56 @@
 // *********variables
+
 let inputIncome = document.getElementById("add-income")
 let inputAddButton = document.getElementById("button-addon2")
 let expensesType = document.getElementById("choose")
 let dateTime = document.getElementById("date-time")
 let amount = document.getElementById("amount")
 let saveButton = document.getElementById("save")
+let tableElement = document.getElementById("list-table")
+
+let resultIncomesSpan = document.getElementById("result-incomes")
 
 let jSonArr = [];
 let jSonObj ;
+// let tableRow;
 
 // *************events listeners
+
 inputAddButton.addEventListener("click",() => {
     console.log(inputIncome.value);
-    // there should be a function name for add result area
+    addIncomesToResult()
 });
 
 saveButton.addEventListener("click",(e) => {
     e.preventDefault()
-
-    let jSonObj = {
+    jSonObj = {
         dt:dateTime.value,
         exp:expensesType.value,
         amnt:amount.value,
     };
     jSonArr.push(jSonObj);
     console.log(jSonArr);
-
+    addExpensesToList()
 });
+
+
+// ************* Functions area 
+
+const addIncomesToResult = () => {
+    resultIncomesSpan.innerText = Number(resultIncomesSpan.innerText) + Number(inputIncome.value)
+}
+const addExpensesToList = () => {
+    
+    let tableRow = `
+    <tbody class="">
+            <tr>
+              <td>${jSonObj.dt}</td>
+              <td>${jSonObj.exp}</td>
+              <td>${jSonObj.amnt}</td>
+              <td><i class="bi bi-trash3 text-danger"></i></td>
+            </tr>
+          </tbody>
+    `;
+    let newTableRow = document.createElement(tableRow)
+    document.getElementById("table-body").appendChild(newTableRow)
+}
